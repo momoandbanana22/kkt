54951fb5da013e71864d693fd633ba2a9e734970
momoandbanana22 <momoandbanana@protonmail.com>
2018-04-19 21:16:49 +0900

    設定ファイルで指定された時間（秒数）ごとに、買う（買ったつもり）動作を行う

--- kkt.rb
+++ kkt.rb
@@ -1,9 +1,10 @@
 # kkt.rb - 'kotsukotsuto' - dollar cost averaging bot
-PROGRAM_VERSION = 'ver.20180411_0645'.freeze
+PROGRAM_VERSION = 'ver.20180419_1012'.freeze
 PROGRAM_NAME = 'kkt'.freeze
 
 # standerd library require
 require 'yaml'
+require 'date'
 
 # relateve file
 require_relative 'kkt_logger.rb'
@@ -15,5 +16,34 @@ SETTING = YAML.load_file('setting.yaml')
 LOG = KktLog.new(SETTING['log']['filepath'])
 LOG.enable = SETTING['log']['enable']
 
-# write info of prgram start.
+# write info of program start.
 LOG.info(object_id, 'main', 'main', (PROGRAM_NAME + ' ' + PROGRAM_VERSION))
+
+def alreadybuy(current_unixtime, interval)
+  lastbuy = YAML.load_file('lastbuy.yaml')
+  return false if lastbuy.nil?
+  return false if lastbuy['unixtime'].nil?
+  lastbuy_datetime = lastbuy['unixtime'].to_i.div(interval)
+  current_unixtime = current_unixtime.div(interval)
+  (lastbuy_datetime == current_unixtime) # return true/false
+end
+
+def buy
+  lastbuytime = Time.now
+  puts lastbuytime
+  lastbuy = {}
+  lastbuy['unixtime'] = lastbuytime.to_i
+  lastbuy['coinname'] = 'xrp'
+  lastbuy['amout'] = 1
+  lastbuy['price'] = 100
+  File.open('lastbuy.yaml', 'w') do |f|
+    YAML.dump(lastbuy, f)
+  end
+  LOG.debug(object_id, self.class.name, __method__, '買ったふり')
+end
+
+loop do
+  current_unixtime = Time.now.to_i
+  buy unless alreadybuy(current_unixtime, SETTING['interval'])
+  sleep(0.5)
+end
