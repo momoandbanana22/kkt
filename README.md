97a500222c824457c094440da44051d85b783b08
momoandbanana22 <momoandbanana@protonmail.com>
2018-04-28 11:08:39 +0900

    bitbankにアクセスして残高を表示

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # kkt - こつこつ、と。
 -----
 このプログラムは、一定期間（例えば１時間に一回とか。設定可能）に、一定の金額（たとえば１０００円分とか。設定可能）の暗号通貨（btcとかxrpとか。設定可能）を、[ビットバンク](https://bitbank.cc/) で買うためのプログラムです。
-現在は作成中のため、まだ買えません。現在のところ、一定時間（例えば１時間に一回とか。設定可能）ごとに、ログファイルに「買ったふり」と出力し、画面にその時間を表示します。停止するにはCTRL+Cです。
+現在は作成中のため、まだ買えません。現在のところ、bitbankにアクセスして残高を表示するだけです。
+
 # はじめに
+無保証です。
+
+# 使い方
+apikey.yamlというファイルを作成して、bitbankのAPIキーを設定し、kkt.rbを実行してください。
+
+下記にapikey.yamlのサンプルを示します。
+~~~yaml
+apikey: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+seckey: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+~~~
+
+# おわりに
 無保証です。
--- kkt.rb
+++ kkt.rb
@@ -1,10 +1,13 @@
 # kkt.rb - 'kotsukotsuto' - dollar cost averaging bot
-PROGRAM_VERSION = 'ver.20180427_2220'.freeze
+PROGRAM_VERSION = 'ver.20180428_1100'.freeze
 PROGRAM_NAME = 'kkt'.freeze
 
 # standerd library require
 require 'yaml'
 require 'date'
+
+# use gem
+require 'ruby_bitbankcc'
 
 # relateve file
 require_relative 'kkt_logger.rb'
@@ -19,19 +22,20 @@ LOG.enable = SETTING['log']['enable']
 # write info of program start.
 LOG.info(object_id, 'main', 'main', (PROGRAM_NAME + ' ' + PROGRAM_VERSION))
 
+BASE_COINNAME = SETTING['base_coin']['coin_name']
+TARGET_COINNAME = SETTING['target_coin']['coin_name']
+
 # read coinpair/side from setting file
 def coinpair_and_side
   # read setting
   bitbank_coinpair = SETTING['bitbank_coinpair']
-  base_coinname = SETTING['base_coin']['coin_name']
-  target_coinname = SETTING['target_coin']['coin_name']
 
   # check pair BASE_TARGET
-  pair = base_coinname + '_' + target_coinname
+  pair = BASE_COINNAME + '_' + TARGET_COINNAME
   return [pair, 'sell'] if bitbank_coinpair.include?(pair)
 
   # check pair TARGET_BASE
-  pair = target_coinname + '_' + base_coinname
+  pair = TARGET_COINNAME + '_' + BASE_COINNAME
   return [pair, 'buy'] if bitbank_coinpair.include?(pair)
 
   # not found
@@ -40,11 +44,45 @@ end
 
 coinpair, side = coinpair_and_side
 if coinpair.nil?
-  LOG.error(object_id, 'main', 'main', 'no coinpair found.')
-  puts('コインペアが見つかりません')
+  LOG.error(object_id, 'main', 'main', 'no coinpair found. program end.')
+  puts('設定ファイルに記述されたコインペアが正しくありません。プログラムを終了しました。')
   exit(-1)
 end
 LOG.debug(object_id, 'main', 'main', 'coinpair=' + coinpair + ' side=' + side)
+
+# initialize Bitbankcc Class
+APIKEY = YAML.load_file('apikey.yaml')
+BBCC = Bitbankcc.new(APIKEY['apikey'], APIKEY['seckey'])
+
+def raw_read_balance
+  res = JSON.parse(BBCC.read_balance())
+  if res['success'] != 1
+    errstr = 'BBCC.read_balance() not success. code=' + res['data']['code'].to_s
+    LOG.error(object_id, self.class.name, __method__, errstr)
+    return nil
+  end
+  res # return res
+rescue => exception
+  LOG.fatal(object_id, self.class.name, __method__, exception.to_s)
+  nil # return nil
+end
+
+def read_amount
+  res = raw_read_balance
+  ret = Hash.new { |h, k| h[k] = {} }
+  res['data']['assets'].each do |one_asset|
+    one_asset.each do |key, val|
+      ret[one_asset['asset']][key] = val if key != 'asset'
+    end
+  end
+  ret # return ret
+end
+
+def free_amout(target_coin)
+  read_amount[target_coin]['free_amount']
+end
+
+puts(BASE_COINNAME + ' の残高は ' + free_amout(BASE_COINNAME).to_s + ' です。')
 
 def alreadybuy(current_unixtime, interval)
   lastbuy = YAML.load_file('lastbuy.yaml')
@@ -69,8 +107,8 @@ def buy
   LOG.debug(object_id, self.class.name, __method__, '買ったふり')
 end
 
-loop do
-  current_unixtime = Time.now.to_i
-  buy unless alreadybuy(current_unixtime, SETTING['interval'])
-  sleep(0.5)
-end
+# loop do
+#  current_unixtime = Time.now.to_i
+#  buy unless alreadybuy(current_unixtime, SETTING['interval'])
+#  sleep(0.5)
+# end
